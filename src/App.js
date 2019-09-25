447d21c45cbc89d4ba2454ba2710e3f493d4e000
vitaliy-kozakevich <kozakevich.vitaliy@gmail.com>
2019-09-25 09:47:04 +0300

    Template with create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Fighters from 'containers/fighters'
+
+const store = createStore( reducers, composeWithDevTools(
+  applyMiddleware(thunk) 
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+        <Route path='/' component={Fighters} />
+      </Route>
+    </Router>
+  </Provider>
   );
 }
 
