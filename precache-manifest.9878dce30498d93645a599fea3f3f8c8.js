fa2bef3edbabbd2273c05e198b56734647d573c1
vitaliy-kozakevich <kozakevich.vitaliy@gmail.com>
2019-09-25 10:39:50 +0300

    Updates

---
+++ precache-manifest.9878dce30498d93645a599fea3f3f8c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33679b9154722cb6f7e6ad6c01bff58",
+    "url": "/street-fighter-react-redax/index.html"
+  },
+  {
+    "revision": "9051a5fdda3946c2f6e1",
+    "url": "/street-fighter-react-redax/static/css/main.4675403e.chunk.css"
+  },
+  {
+    "revision": "17be253ac840e92070b9",
+    "url": "/street-fighter-react-redax/static/js/2.32538351.chunk.js"
+  },
+  {
+    "revision": "9051a5fdda3946c2f6e1",
+    "url": "/street-fighter-react-redax/static/js/main.b805af5c.chunk.js"
+  },
+  {
+    "revision": "1a5f9701229f78f01e85",
+    "url": "/street-fighter-react-redax/static/js/runtime-main.6ddeaf5b.js"
+  }
+]);
