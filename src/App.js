d5f926ed09602be15d2b8d0b726e7554b04a3f29
Karim Aziiev <karim.aziev@gmail.com>
2019-10-04 11:11:17 +0300

    Code review notes:
    The node_modules folder should contains libraries downloaded from npm,
    so pluck everything from the one in the src directory.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import { Provider } from 'react-redux'
-import { Router, Route } from 'react-router'
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { browserHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
+import { Provider } from "react-redux";
+import { Router, Route } from "react-router";
 
-import reducers from 'reducers'
-import Layout from 'containers/layout'
-import Fighters from 'containers/fighters'
+import reducers from "reducers";
+import Layout from "containers/layout";
+import Fighters from "containers/fighters";
 
-const store = createStore( reducers, composeWithDevTools(
-  applyMiddleware(thunk) 
-))
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
 function App() {
   return (
     <Provider store={store}>
-    <Router history={history}>
-      <Route component={Layout}>
-        <Route path='/' component={Fighters} />
-      </Route>
-    </Router>
-  </Provider>
+      <Router history={history}>
+        <Route component={Layout}>
+          <Route path="/" component={Fighters} />
+        </Route>
+      </Router>
+    </Provider>
   );
 }
 
