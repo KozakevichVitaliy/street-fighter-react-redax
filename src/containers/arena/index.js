a511fe226533d7c15054a3d4df35f59127e5bb4d
KozakevichVitaliy <samcroc2012@gmail.com>
2019-10-18 01:19:16 +0300

    set arena  background-img

--- src/containers/arena/index.js
+++ src/containers/arena/index.js
@@ -7,11 +7,36 @@ class Arena extends Component {
   componentDidMount() {
   }
   
-  render() {
-   
+  renderFighter(fighter, index) {
+    const reverseFighter = (index) => {
+      if (index === 0) {
+        return "fighter"
+      } else {
+        return "fighter"
+      }
+    }
     return (
-      <div>
-        Arena
+      // index as a key is an anti-pattern espessialy when you have stable IDs
+      // https://reactjs.org/docs/lists-and-keys.html
+      
+      <div className="fighter" key={index}>
+        <img
+          className="fighter-image"
+          src={fighter.source}
+          alt={fighter.source}
+        />
+        <span className="name">{fighter.name}</span>
+      </div>
+    );
+  }
+
+  render() {
+    const { arena } = this.props
+    return (
+      <div className="arena-background">
+        <div className="arena-fighters">
+          {arena.map(this.renderFighter)}
+        </div>
       </div>
       );
   }
